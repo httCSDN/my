30e6d2e3e0332eb4253efa7b4f6df464049f3603
huangtingting <2842759004@qq.com>
2021-03-17 10:21:33 +0800

    类

---
+++ car.py
@@ -0,0 +1,43 @@
+class Car():
+    def __init__(self,make,model,year):
+      """
+      初始化描述汽车的属性
+      """
+      self.make=make
+      self.model=model
+      self.year=year
+      #给属性指定默认值
+      self.odometer_reading=0
+    def get_descriptive_name(self):
+        long_name=str(self.year)+" "+self.make+" "+self.model
+        return  long_name.title()
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+    def update_odometer(self,mileage):
+        """
+        将里程表读数设置成指定的值
+        禁止将里程表读数往回调
+        :param mileage:
+        :return:
+        """
+        if mileage>=self.odometer_reading:
+            self.odometer_reading=mileage
+        else:
+            print("You cant roll back on odometer")
+    def increment(self,mils):
+        """
+        将里程表读数增加指定的量
+        :param mils:
+        :return:
+        """
+        self.odometer_reading+=mils
+
+
+
+
+
+
+
+
+
+
---
+++ car_c.py
@@ -0,0 +1,28 @@
+from car import *
+"""
+根据car类创建
+"""
+# my_new_car=Car("奥迪",'a4',"2018")
+# print(my_new_car.get_descriptive_name())
+# my_new_car.read_odometer()
+"""
+直接修改属性值
+"""
+# my_new_car.odometer_reading=23
+# my_new_car.read_odometer()
+"""
+通过方法修改属性值
+"""
+mile=input("请输入里程数:")
+"""
+判断输入的里程数大于等于当前的里程数，通过修改里程数，并且打印；不通过，提示错误信息
+"""
+# if int(mile)>=int(my_new_car.odometer_reading):
+#     my_new_car.update_odometer(int(mile))
+#     my_new_car.read_odometer()
+# else:
+#     my_new_car.update_odometer(int(mile))
+
+
+# my_new_car.increment(int(input("请输入增加的指定量")))
+# my_new_car.read_odometer()
